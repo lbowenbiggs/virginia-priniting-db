12303920f6e0880b025289f248c768c26910ca1a
Leo Bowen-Biggs <yttrium.lead@gmail.com>
2017-10-24 16:45:20 -0400

    Define some models to minimally capture Rawson's data

--- VirginiaPrinting/models.py
+++ VirginiaPrinting/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class Record(models.Model):
+    title = models.CharField(max_length=200)
+    pdf_location = models.FilePathField()
+    record_type = models.CharField(max_length=100)
+
+
+# The next 4 models are an alternative to above based on the four PDF types Rawson gave us
+class NewspaperCitation(models.Model):
+    title = models.CharField(max_length=200)
+    num_variants = models.IntegerField()
+    pdf_location=models.FilePathField()
+
+
+class NewspaperHistory(models.Model):
+    pdf_location = models.FilePathField()
+    newspaper_citation = models.ForeignKey(NewspaperCitation, on_delete=models.CASCADE)
+
+
+class Biography(models.Model):
+    name = models.CharField(max_length=200)
+    pdf_location = models.FilePathField()
+
+
+class ImprintRecord(models.Model):
+    year = models.IntegerField()
+    sequence_number = models.IntegerField()
+    short_title = models.CharField(max_length=200)
+    pdf_location = models.FilePathField()
