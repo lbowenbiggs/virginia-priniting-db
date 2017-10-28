ac275065ef3122f8503e5808a53858087dc47881
Leo Bowen-Biggs <yttrium.lead@gmail.com>
2017-10-28 19:48:15 -0400

    Create basic 'detail' page for Newspaper Citations
    Also, rearranged static files to serve them properly.
    
    Part of #4

--- VirginiaPrinting/urls.py
+++ VirginiaPrinting/urls.py
@@ -5,4 +5,6 @@ app_name = 'VPDB'
 urlpatterns = [
     # ex: /VPDB/
     url(r'^$', views.index, name='index'),
+    # ex: /VPDB/news_cite/1/
+    url(r'^news_cite/(?P<obj_id>[0-9]+)/$', views.news_cite_detail, name='news_cite_detail')
 ]
--- VirginiaPrinting/views.py
+++ VirginiaPrinting/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import NewspaperCitation
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, Virginia")
+
+
+def news_cite_detail(request, obj_id):
+    newspaper_citation = get_object_or_404(NewspaperCitation, pk=obj_id)
+    return render(request, 'VirginiaPrinting/newspaper_citation.html', {'newspaper_citation': newspaper_citation})
