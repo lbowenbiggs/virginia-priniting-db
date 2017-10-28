5078d0fb439cd1818002d464ef780180d6870b4d
Leo Bowen-Biggs <yttrium.lead@gmail.com>
2017-10-27 23:32:02 -0400

    Define __str__ on models for readability; constrain where pdfs can be located

--- VirginiaPrinting/models.py
+++ VirginiaPrinting/models.py
@@ -5,21 +5,33 @@ from django.db import models
 class NewspaperCitation(models.Model):
     title = models.CharField(max_length=200)
     num_variants = models.IntegerField()
-    pdf_location=models.FilePathField()
+    pdf_location=models.FilePathField(path="~/PycharmProjects/VPDB/static/journal_citations")
+
+    def __str__(self):
+        return "Citation: " + self.title
 
 
 class NewspaperHistory(models.Model):
-    pdf_location = models.FilePathField()
+    pdf_location = models.FilePathField(path="~/PycharmProjects/VPDB/static/journal_histories")
     newspaper_citation = models.ForeignKey(NewspaperCitation, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "History: " + self.newspaper_citation.title
 
 
 class Biography(models.Model):
     name = models.CharField(max_length=200)
-    pdf_location = models.FilePathField()
+    pdf_location = models.FilePathField(path="~/PycharmProjects/VPDB/static/biographies")
+
+    def __str__(self):
+        return self.name
 
 
 class ImprintRecord(models.Model):
     year = models.IntegerField()
     sequence_number = models.IntegerField()
     short_title = models.CharField(max_length=200)
-    pdf_location = models.FilePathField()
+    pdf_location = models.FilePathField(path="~/PycharmProjects/VPDB/static/imprints")
+
+    def __str__(self):
+        return str(self.sequence_number) + ": " + self.short_title
