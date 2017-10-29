aab87ca17d736a06b0ead3310998b2173979cb18
Leo Bowen-Biggs <yttrium.lead@gmail.com>
2017-10-28 21:08:32 -0400

    Add a page to hold newspaper histories.
    Part of #4

--- VirginiaPrinting/urls.py
+++ VirginiaPrinting/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # ex: /VPDB/
     url(r'^$', views.index, name='index'),
     # ex: /VPDB/news_cite/1/
-    url(r'^news_cite/(?P<pk>[0-9]+)/$', views.NewsCiteDetailView.as_view(), name='news_cite_detail')
+    url(r'^news_cite/(?P<pk>[0-9]+)/$', views.NewsCiteDetailView.as_view(), name='news_cite_detail'),
+    # ex: /VPDB/news_hist/1/
+    url(r'^news_hist/(?P<pk>[0-9]+)/$', views.NewsHistDetailView.as_view(), name='news_hist_detail'),
 ]
--- VirginiaPrinting/views.py
+++ VirginiaPrinting/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.views import generic
 
-from .models import NewspaperCitation
+from .models import NewspaperCitation, NewspaperHistory
 
 
 # Create your views here.
@@ -13,3 +13,9 @@ class NewsCiteDetailView(generic.DetailView):
     model = NewspaperCitation
     context_object_name = 'newspaper_citation'
     template_name = 'VirginiaPrinting/newspaper_citation.html'
+
+
+class NewsHistDetailView(generic.DetailView):
+    model = NewspaperHistory
+    context_object_name = 'newspaper_history'
+    template_name = 'VirginiaPrinting/newspaper_history.html'
