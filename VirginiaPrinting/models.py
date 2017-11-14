47affa7f86c3b8dcea60b8518fd5a67fcf27d8bf
Leo Bowen-Biggs <leo.bowen.biggs@gmail.com>
2017-11-14 16:52:48 -0500

    Expand models to caputure more information on Rawson's data

--- VirginiaPrinting/models.py
+++ VirginiaPrinting/models.py
@@ -1,38 +1,72 @@
 from django.db import models
 
 
-# These models correspond to the four types of documents Professor Rawson gave us
-class NewspaperCitation(models.Model):
-    title = models.CharField(max_length=200)
-    num_variants = models.IntegerField()
-    pdf_location=models.FilePathField(path="~/PycharmProjects/VPDB/static/journal_citations")
-
-    def __str__(self):
-        return "Citation: " + self.title
-
-
-class NewspaperHistory(models.Model):
-    pdf_location = models.FilePathField(path="~/PycharmProjects/VPDB/static/journal_histories")
-    newspaper_citation = models.ForeignKey(NewspaperCitation, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return "History: " + self.newspaper_citation.title
-
-
+# Expanded Models
 class Biography(models.Model):
-    name = models.CharField(max_length=200)
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    formal_name = models.CharField(max_length=50, blank=True)
+    function = models.CharField(max_length=200, default="Unknown")
+    locales = models.CharField(max_length=200, default="Virginia")
+    precis = models.TextField(blank=True)
+    first_date = models.IntegerField(blank=True)
+    last_date = models.IntegerField(blank=True)
+
+    associates = models.ManyToManyField("self")
+
     pdf_location = models.FilePathField(path="~/PycharmProjects/VPDB/static/biographies")
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = "biographies"
 
 
 class ImprintRecord(models.Model):
     imprint_number = models.DecimalField(primary_key=True, max_digits=7, decimal_places=3)
     year = models.IntegerField()
     sequence_number = models.IntegerField()
-    short_title = models.CharField(max_length=200)
+    short_title = models.CharField(max_length=200, blank=True)
+    author = models.CharField(max_length=200, blank=True)
+    title = models.TextField(blank=True)
+    place_issued = models.CharField(max_length=200, default="Virginia")
+    issuing_press = models.CharField(max_length=200, default="Uncertain")
+    description = models.CharField(max_length=200, default="Unrecorded")
+
+    associates = models.ManyToManyField(Biography)
+
     pdf_location = models.FilePathField(path="static/imprints")
 
     def __str__(self):
         return str(self.imprint_number) + ": " + self.short_title
+
+
+class NewspaperHistory(models.Model):
+    lineage_number = models.CharField(primary_key=True, max_length=200)
+    group_title = models.CharField(max_length=200, blank=True)
+
+    pdf_location = models.FilePathField(path="~/PycharmProjects/VPDB/static/journal_histories")
+
+    def __str__(self):
+        return self.group_title
+
+    class Meta:
+        verbose_name_plural = "newspaper histories"
+
+
+class NewspaperCitation(models.Model):
+    variant_number = models.CharField(primary_key=True, max_length=200)
+    title = models.CharField(max_length=200, blank=True)
+    start_date = models.CharField(max_length=200, blank=True)
+    end_date = models.CharField(max_length=200, blank=True)
+    frequency = models.CharField(max_length=200, blank=True)
+    proprietors = models.CharField(max_length=200, default="Unknown")
+
+    lineage = models.ForeignKey(NewspaperHistory, blank=True, null=True)
+    associates = models.ManyToManyField(Biography)
+
+    pdf_location=models.FilePathField(path="~/PycharmProjects/VPDB/static/journal_citations")
+
+    def __str__(self):
+        return self.title
